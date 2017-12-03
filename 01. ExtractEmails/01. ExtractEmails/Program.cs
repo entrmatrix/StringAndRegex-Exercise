001a96f449a70efed9c5c59a581119d9e66d5ee0
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2017-12-03 12:48:12 +0100

    Added one commentary to the project.

---
+++ 01. ExtractEmails/01. ExtractEmails/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _01.ExtractEmails
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            //with this regex I am extracting all email address from a given text
+            string pattern = @"(?<=\s|^)([a-z0-9]+(?:[_.-][a-z0-9]+)*@(?:[a-z]+\-?[a-z]+\.)+[a-z]+\-?[a-z]+)\b";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
